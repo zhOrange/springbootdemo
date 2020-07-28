c2760e88030f99f84aaaeb93d29869d7cbc45ebd
ebs706ar@163.com <jian0706_zh@163.com>
2020-07-28 17:46:19 +0800

    edit sth

--- src/main/java/com/zh/springbootdemo/web/controller/HelloWorldController.java
+++ src/main/java/com/zh/springbootdemo/web/controller/HelloWorldController.java
@@ -3,7 +3,10 @@ package com.zh.springbootdemo.web.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //COntroller + responseController
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController //Controller + responseController
 public class HelloWorldController {
     @RequestMapping("/hello")
     public String getHello(){
@@ -13,6 +16,22 @@ public class HelloWorldController {
     @RequestMapping("")
     public String info(){
         return "Info";
+    }
+
+    @RequestMapping("Time")
+    public String getTime(String time){
+        switch(time){
+            case "time":
+                // http://127.0.0.1:8080/Time?time=time
+                SimpleDateFormat tFormat = new SimpleDateFormat("HH:mm:ss");
+                return "当前时间为： " + tFormat.format(new Date());
+            case "date":
+                // http://127.0.0.1:8080/Time?time=date
+                SimpleDateFormat dFormat = new SimpleDateFormat("yy-MM-dd");
+                return "当前日期为： " + dFormat.format(new Date());
+                default:
+                    return "";
+        }
     }
 
 }
